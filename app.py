b0a6bf13f5c3882eed3eaf2dc13db61c47aee42f
Rebecca Hagerty <rebecca.hagerty12@gmail.com>
2021-01-19 14:38:46 -0500

    updated app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///mixology"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-app.config["SECRET_KEY"] = "abc123"
+app.config["SECRET_KEY"] = "nevertell"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
