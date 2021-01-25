6ece5c4b115900c724bb1af0223d9fda8faf1cc5
Rebecca Hagerty <rebecca.hagerty12@gmail.com>
2021-01-25 10:46:21 -0500

    updated db

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgres:///mixology')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "nevertell"
